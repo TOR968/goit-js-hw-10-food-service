28d87cd5bc977954b3777040a1394bc9fef758eb
Maxim Turbal <tor968@yahoo.com>
2020-10-07 20:03:30 +0300

    Update theme.js

--- src/theme.js
+++ src/theme.js
@@ -1,28 +1,28 @@
-// class="dark-theme"
+
 const Theme = {
-  LIGHT: "light-theme",
-  DARK: "dark-theme",
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
 };
-const swichToggle = document.querySelector("#theme-switch-toggle");
-const bodyEl = document.querySelector("body");
+const swichToggle = document.querySelector('#theme-switch-toggle');
+const bodyEl = document.querySelector('body');
 
-swichToggle.addEventListener("change", onSwich);
+swichToggle.addEventListener('change', onSwich);
 
-bodyEl.setAttribute("class", localStorage.getItem("Theme"));
+bodyEl.setAttribute('class', localStorage.getItem('Theme'));
 
-if (localStorage.getItem("Theme") === Theme.DARK) {
+if (localStorage.getItem('Theme') === Theme.DARK) {
   swichToggle.checked = true;
 }
 
 function onSwich(e) {
-  if (e.target.checked === true) {
+  if (e.target.checked) {
     bodyEl.classList.add(Theme.DARK);
     bodyEl.classList.remove(Theme.LIGHT);
-  }
-  if (e.target.checked === false) {
+    localStorage.setItem('Theme', Theme.DARK);
+  } else {
     bodyEl.classList.add(Theme.LIGHT);
     bodyEl.classList.remove(Theme.DARK);
+    localStorage.setItem('Theme', Theme.LIGHT);
   }
-
-  localStorage.setItem("Theme", bodyEl.getAttribute("class"));
 }
+

c4f11f29c4d8c29f0d0001ba5d5a665756801a52
Maxim <Tor968@yahoo.com>
2020-10-07 08:55:05 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
 import cards from "./card.hbs";
+import menu from "./menu.json";
+
+const menuContainer = document.querySelector("js-menu");
+const cardsMarkup = createMenuMarkup(menu);
+
+menuContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+
+function createMenuMarkup(menu) {
+  //   return menu.map(cards).join("");
+  return cards(menu);
+}
